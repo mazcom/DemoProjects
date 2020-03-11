ae82d2fa1ecc9b9f40a639010438ffbe3b9aabd9
VitaliiZ <vitaliiz@devart.com>
2020-03-11 14:03:49 +0200

    add procedures

---
+++ DemoAccounting/ScriptsFolder/Programmability/Procedures/dbo.GetCustomers.sql
@@ -0,0 +1,6 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[GetCustomers]    
+AS   
+    SELECT * FROM Customer    
+GO
---
+++ DemoAccounting/ScriptsFolder/Programmability/Procedures/dbo.GetVendors.sql
@@ -0,0 +1,6 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[GetVendors]    
+AS   
+    SELECT * FROM Vendor    
+GO
