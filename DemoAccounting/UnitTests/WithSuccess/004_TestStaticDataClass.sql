f38209bd8b11112a8dc4542caab4ef928b20ba35
VitaliiZ <vitaliiz@devart.com>
2020-03-11 17:00:59 +0200

    add unit tests

---
+++ DemoAccounting/UnitTests/WithSuccess/004_TestStaticDataClass.sql
@@ -0,0 +1,5 @@
+﻿CREATE SCHEMA [TestStaticDataClass] AUTHORIZATION [dbo]
+GO
+
+EXEC sys.sp_addextendedproperty N'tSQLt.TestClass', 1, 'SCHEMA', N'TestStaticDataClass'
+GO
---
+++ DemoAccounting/UnitTests/WithSuccess/006_TestStaticDataClass.test TestPaymentTypes.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [TestStaticDataClass].[test TestPaymentTypes]
+AS
+BEGIN
+  -- Here must be your logic
+  -- We just return OK for simplification purposes
+  EXEC tSQLt.AssertEquals @Expected = 1, @Actual = 1
+END;
+GO
